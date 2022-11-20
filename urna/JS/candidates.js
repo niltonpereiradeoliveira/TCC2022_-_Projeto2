ab79b914089c9ed92f8fd48d3eadba8569a32496
NILTON OLIVEIRA <003.oliveira@gmail.com>
2022-11-20 09:44:01 -0300

    Inicio

---
+++ urna/JS/candidates.js
@@ -0,0 +1,40 @@
+let candidates = [
+    {
+        currenty: 'Candidato à CIPA',
+        number: 1,
+        candidate: [
+            {
+                ncandidate: 10,
+                nname: 'CICLANO',
+                ssector: 'Informatica',
+                images:[
+                    {url:'13.png', legend:'Candidato à CIPA'}
+                ]
+            },
+            {
+                ncandidate: 23,
+                nname: 'FULANO',
+                ssector: 'Bazar',
+                images:[
+                    {url:'23.png', legend:'Candidato à CIPA'}
+                ]
+            },
+            {
+                ncandidate: 33,
+                nname: 'BELTRANO',
+                ssector: 'Recebimento',
+                images:[
+                    {url:'33.png', legend:'Candidato à CIPA'}
+                ]
+            },
+            {
+                ncandidate: 44,
+                nname: 'URÂNIA',
+                ssector: 'Recepção',
+                images:[
+                    {url:'44.png', legend:'Candidata à CIPA'}
+                ]
+            },
+        ]
+    }
+]
